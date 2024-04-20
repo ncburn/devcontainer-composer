fa9dbb714f5e3bac10adba98df3e804dd4bce63a
ncburn <nickburnard@outlook.com>
2024-04-19 22:07:24 -0400

    Moved apt-get code from Containerfile to install_debian.sh script file

---
+++ scripts/container/install_debian.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update -y && \
+apt-get upgrade -y && \
+apt-get install -q -y --no-install-suggests \
+    ca-certificates \
+    openssh-client \
+    openssl \
+    curl \
+    zip \
+    unzip \
+    sudo \
+    git \
+    zsh
